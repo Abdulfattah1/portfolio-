8c1e1544882206bd8ab6346a9a40c5d4dc7a28b7
a0952432706 <abdulfattah.khudari@gmail.com>
2019-01-24 12:51:26 +0200

    the first project is added

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const server = http.createServer(app);
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/project/:id", (req, res, next) => {
+  const Id = req.params.id;
+  const fileName = "pro" + Id;
+  res.render(fileName);
+});
+
 app.use("/", (req, res, next) => {
   res.render("index.ejs");
 });
